e727e178d895960fd22eff5aea5f43fe8064a541
Bisma-Anwer <bismaanwer2001@gmail.com>
2026-02-21 06:46:23 -0800

    done

---
+++ chapter 1/app.js
@@ -0,0 +1,20 @@
+// answer no:1
+
+// alert ("Hy Welcome to the Website!") ;
+
+// answer no:2
+
+// alert ("Error! Please enter a valid password") ; 
+
+// answer no:3
+
+// alert ("Welcome to JS land... \nHappy Coding!")
+
+// answer no:4
+
+// alert ("Welcome to JS land...")
+// alert ("Happy Coding!")
+
+// answer no:5
+
+// console.log ("Hello... I can run JS through my web browser's console")
