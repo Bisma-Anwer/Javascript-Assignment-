208bef7765e27c98c21bd53e031bd2f9b15a3693
Bisma-Anwer <bismaanwer2001@gmail.com>
2026-02-24 06:57:13 -0800

    all done

---
+++ chapter 2/app.js
@@ -0,0 +1,49 @@
+// answer 1
+
+// let username = "Bisma"
+
+// answer 2
+
+// let myName = "Bisma Anwer"
+
+// answer 3
+
+// let message = "Hello World"
+// alert(message)
+
+// answer 4
+
+// let studentName = "Jhone Doe"
+// alert (studentName)
+
+
+// let studentAge = "15 years old"
+// alert (studentAge)
+
+// let studentCourse = "Modern Web and App Development "
+// alert (studentCourse)
+
+
+// answer 5
+
+// let content = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(content)
+
+// answer 6
+
+// let email = "bismaanwer200@gmail.com"
+// alert ("My email address is " + email)
+
+// answer 7
+
+// let book = "A smarter way to learn JavaScript"
+// alert ("I am trying to learn from the Book " + book)
+
+// answer 8
+
+// document.write ("Yah! I can write HTML content through JavaScript")
+
+// answer 9
+
+// let design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(design)
