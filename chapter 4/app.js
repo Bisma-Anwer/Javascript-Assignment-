58f1c1ce6d6e36aabbfc82f0d0b097d43bb5684a
Bisma-Anwer <bismaanwer2001@gmail.com>
2026-03-08 17:58:34 +0500

    done

---
+++ chapter 4/app.js
@@ -0,0 +1,29 @@
+// answer 1
+// let a,b,c; 
+
+// answer 2
+// 5 legal
+// let Name = "Bisma";
+// let fatherName = "Anwer";
+// let _currently_learning = "Web Development";
+// let qualification1 = "Graduate";
+// let $city = "Karachi";
+
+// 5 illegal
+// let 1Name = "Bisma";
+// let father name = "Anwer";
+// let class = "Web Development";
+// let @qualification = "Graduate";
+// let ci-ty = "Karachi";
+
+// answer 3
+// document.write("<h2> “Rules for naming JS variables” </h2> ")
+// document.write("Variable names can only contain ,number, $, and _ . For example: $my_1stVariable")
+// document.write("<br/>")
+// document.write("Variables must begin with a letter, $ or _ . For example: $name, _name or name")
+// document.write("<br/>")
+// document.write("Variable names are case sensitive")
+// document.write("<br/>")
+// document.write("Variable names should not be JS keywords")
+
+
