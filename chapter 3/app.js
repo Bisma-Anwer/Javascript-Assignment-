091811aa42dd40d0881ad48409e196ac69000b8a
Bisma-Anwer <bismaanwer2001@gmail.com>
2026-03-08 16:54:22 +0500

    done

---
+++ chapter 3/app.js
@@ -0,0 +1,24 @@
+// // answer 1
+
+// let age = 15
+// alert("I am " + age + "years old");
+
+// answer 2
+// let visits = 14
+
+// alert("You have visited this site " + visits + " times")
+
+// answer 3
+// let birthYear = 1990
+
+// document.write("My birth year is " + birthYear)
+// document.write("<br/>")
+// document.write("Data type of my declared variable is number")
+
+// // answer 4
+
+// let visitorsName = "John Doe"
+// let productTitle = "XYZ Clothing store"
+// let quantity = "5 T-shirt(s)"
+
+// document.write(visitorsName + " ordered " + quantity + " on " +productTitle)
