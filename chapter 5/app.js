27b38785b4bc96c30ea7b0b12ce8236a2e857e6b
Bisma-Anwer <bismaanwer2001@gmail.com>
2026-03-08 13:11:37 +0500

    done

---
+++ chapter 5/app.js
@@ -0,0 +1,137 @@
+// answer no 1
+
+// let num1 = 3;
+// let num2 = 5;
+
+// let ans = num1 + num2;
+// document.write(`Sum of ${num1} and ${num2} is ${ans}`);
+
+// answer no 2 (subtraction)
+
+// let num1 = 5;
+// let num2 = 3;
+
+// let ans = num1 - num2;
+// document.write(`Subtraction of ${num1} and ${num2} is ${ans}`);
+
+// answer no 2 (multiplication)
+
+// let num1 = 3;
+// let num2 = 5;
+
+// let ans = num1 * num2;
+// document.write(`Multiplication of ${num1} and ${num2} is ${ans}`);
+
+// answer no 2 (multiplication)
+
+// let num1 = 3;
+// let num2 = 5;
+
+// let ans = num1 / num2;
+// document.write(`Division of ${num1} and ${num2} is ${ans}`);
+
+// answer no 2 (modulus)
+
+// let num1 = 5;
+// let num2 = 3;
+
+// let ans = num1 % num2;
+// document.write(`Remainder of ${num1} and ${num2} is ${ans}`);
+
+// answer no 3
+// a
+
+// let num1;
+
+// // b
+
+// document.write(`Value after variable declaration is ${num1}`);
+
+// // c
+
+//  let num2 = 5;
+
+// // d
+// document.write("<br/>")
+// document.write(`Initial value: ${num2}`);
+
+// // e
+
+//  let num3 = 5;
+
+// //  f
+
+// document.write("<br/>")
+// document.write(`Value after increment is: ${++num3}`);
+
+// // g
+// let num = num3 + 7;
+
+// // h
+// document.write("<br/>")
+// document.write(`Value after addition is: ${num}` )
+
+// // i
+// let number= --num
+
+// // j
+// document.write("<br/>")
+// document.write(`Value after decrement is: ${num}` )
+
+// // k
+
+// let numb= num % 3
+
+// // l
+
+// document.write("<br/>")
+// document.write(`The remainder is: ${numb}` )
+
+// answer no 4
+
+// let ticket = 5
+// let fiveTicket = 600*5
+// document.write(`Total cost to buy ${ticket} tickets to a movie is ${fiveTicket}PKR`)
+
+// answer no 5
+
+// let count = 1;
+// let tableNum = 4
+
+// document.write("Table of 4")
+// document.write("<br/>")
+// document.write(`${tableNum} x ${count} = ${tableNum}`)
+// document.write("<br/>")
+// count++
+// document.write(`${tableNum} x ${count} = ${tableNum*count}`)
+// document.write("<br/>")
+// count++
+// document.write(`${tableNum} x ${count} = ${tableNum*count}`)
+// document.write("<br/>")
+// count++
+// document.write(`${tableNum} x ${count} = ${tableNum*count}`)
+// document.write("<br/>")
+// count++
+// document.write(`${tableNum} x ${count} = ${tableNum*count}`)
+// document.write("<br/>")
+// count++
+// document.write(`${tableNum} x ${count} = ${tableNum*count}`)
+// document.write("<br/>")
+// count++
+// document.write(`${tableNum} x ${count} = ${tableNum*count}`)
+// document.write("<br/>")
+// count++
+// document.write(`${tableNum} x ${count} = ${tableNum*count}`)
+// document.write("<br/>")
+// count++
+// document.write(`${tableNum} x ${count} = ${tableNum*count}`)
+// document.write("<br/>")
+// count++
+// document.write(`${tableNum} x ${count} = ${tableNum*count}`)
+
+
+
+
+
+
+
